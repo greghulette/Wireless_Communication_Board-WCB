750d57cf66133c3ff94aa7f881ba611e66909d82
greghulette <greg.hulette@gmail.com>
2025-10-31 08:39:13 -0400

    Update WCB.ino

--- Code/WCB/WCB.ino
+++ Code/WCB/WCB.ino
@@ -82,7 +82,7 @@ bool debugEnabled = false;
 
 // WCB Board HW and SW version Variables
 int wcb_hw_version = 0;  // Default = 0, Version 1.0 = 1 Version 2.1 = 21, Version 2.3 = 23, Version 2.4 = 24, Version 3.0 = 30
-String SoftwareVersion = "5.0_310836RJUL25";
+String SoftwareVersion = "5.0_310836ROCT25";
 
 Preferences preferences;  // Allows you to store information that persists after reboot and after reloading of sketch
 
