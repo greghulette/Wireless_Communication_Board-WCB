cb741396446ba1fc66bf4b0b2add08d4918f2c21
greghulette <greg.hulette@gmail.com>
2026-02-26 09:03:56 -0500

    Update WCB.ino

--- Code/WCB/WCB.ino
+++ Code/WCB/WCB.ino
@@ -26,7 +26,7 @@ ____    __    ____  __  .______       _______  __       _______      _______.   
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///*****                                                                                                        *****////
 ///*****                                          Created by Greg Hulette.                                      *****////
-///*****                                          Version 6.0_251308FEB2026                                    *****////
+///*****                                          Version 6.0_260403RFEB2026                                    *****////
 ///*****                                                                                                        *****////
 ///*****                                 So exactly what does this all do.....?                                 *****////
 ///*****                       - Receives commands via Serial or ESP-NOW                                        *****////
@@ -87,7 +87,7 @@ ____    __    ____  __  .______       _______  __       _______      _______.   
 #include <freertos/queue.h>
 #include "WCB_PWM.h"
 #include "WCB_Help.h"
-
+ 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///*****                                                                                                        *****////
 ///*****                                    DO NOT CHANGE ANYTHING IN THIS FILE                                 *****////
@@ -132,7 +132,7 @@ bool debugPWMEnabled = false;
 bool debugPWMPassthrough = false;  // Debug flag for PWM passthrough operations
 // WCB Board HW and SW version Variables
 int wcb_hw_version = 0;  // Default = 0, Version 1.0 = 1 Version 2.1 = 21, Version 2.3 = 23, Version 2.4 = 24, Version 3.1 = 31, Version 3.2 = 32
-String SoftwareVersion = "6.0_251308FEB2026";
+String SoftwareVersion = "6.0_260403RFEB2026";
 
 // ESP-NOW Statistics
 unsigned long espnowSendAttempts = 0;
